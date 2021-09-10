e5ce6ea17e8afce249b268e7eb28f361c5f3b904
whowoo1 <whowoo1@gamil.com>
2021-09-10 15:52:32 +0900

    last ver1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+// https://yts-proxy.now.sh/list_movies.json
+
 import React from "react";
+import axios from "axios";
+import Movie from "./movie";
+import "./app.css";
 
 
 class App extends React.Component{
@@ -6,17 +11,47 @@ class App extends React.Component{
     isLoading : true,
     movies:[]
   };
-  
+
+/// async를 쓰려면, 선언한 함수안에서 await를 선언해야 한다. 
+/// axios.get을 통해서 api를 가져온다 .
+
+getMovies = async() => {
+  const {data:{data:{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by = rating");
+  this.setState({movies , isLoading : false});
+};
+
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading:false});
-    }, 6000);
+  this.getMovies();
   }
+
+  /// render 하는 내용이 web으로 나오고, 자동으로 html이 업데이트 되면서 실행된다 . 
+  /// ? -> true // : -> false // => 함수선언
   render(){
-    const {isLoading} = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"} </div>;
+    const {isLoading, movies} = this.state;
+    return (
+    <section className="container">
+      {isLoading ? (
+        <div className="loader">
+          <span className ="loader__text">Loading...</span>
+        </div>
+      ): (
+        <div className = "movies">
+            {movies.map(movie => (
+            <Movie 
+                key = {movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image} 
+                genres = {movie.genres} />
+      ))}
+        </div>
+      )}      
+    </section>
+  );
   }
 }
-
+            
 
 export default App;
---
+++ src/movie.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types"
+import "./movie.css";
+
+// movie.js에 Movie 함수를 만들고 타이틀을 return 한다.
+// proptypes를 선언하여, Movie 함수안의 각 요소를 number/string 으로 타입을 정의한다.
+/// 타입이 정의된 Movie함수를 App.js로 보낸다.
+function Moive({year,title,summary,poster,genres}){
+    return (
+    <div className = "movie">        
+        <img src={poster} alt = {title} title ={title}/>
+    <div className = "movie_data">
+        <h3 className ="movie__title">{title}</h3>
+        <h5 className ="movie__year">{year}</h5>
+        <ul className="movie__gneres"> 
+        {genres.map((genres,index) => (<li key ={index} className="genres__gnere">{genres}</li>))}
+        </ul>
+        <p className = "movie__summary">{summary.slice(0,180)}...</p>
+        </div>
+    </div>
+    );
+}
+
+Moive.propTypes = {
+    id : PropTypes.number.isRequired,
+    year : PropTypes.number.isRequired,
+    title : PropTypes.string.isRequired,
+    summary : PropTypes.string.isRequired,
+    poster : PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Moive; 
